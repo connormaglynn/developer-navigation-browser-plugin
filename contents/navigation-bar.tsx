73e39d2bb156de7d2d915a2285091945546cadd9
connormaglynn <connor.glynn@digital.justice.gov.uk>
2022-09-29 23:40:51 +0100

    PP-82: ✨Add a dynamic URL parser

--- contents/navigation-bar.tsx
+++ contents/navigation-bar.tsx
@@ -1,14 +1,11 @@
 import cssText from "data-text:~/contents/navigation-bar.css"
 import type { PlasmoContentScript } from "plasmo"
+import { useState } from "react"
 
 export const config: PlasmoContentScript = {
   matches: ["https://github.com/*", "https://app.circleci.com/pipelines/github/*"],
   css: ["font.css"]
 }
-
-// Idea for an UI API, for popup, notification badge, or mounting UI
-// Idea for static mount
-// Idea for styling injection support (inline or with custom emotion cache)
 
 export const getStyle = () => {
   const style = document.createElement("style")
@@ -16,23 +13,64 @@ export const getStyle = () => {
   return style
 }
 
-const currentUrl = window.location.href
-const resourceArray = currentUrl.split('/')
-const organization = resourceArray[resourceArray.length - 2] || ''
-const project = resourceArray[resourceArray.length - 1] || ''
+class UrlParser {
+  private url: URL
+  private organization: string
+  private project: string
 
-const NavigationBar = () => {
-  return (
-    <div className="navigation-bar" style={{}}>
-      <Button name="Github"  href={"https://github.com/" + organization + "/" + project} className="github button" />
-      <Button name="CircleCI" href={"https://app.circleci.com/pipelines/github/" + organization + "/" + project} className="circleci button" />
-    </div>
-  )
+  public setUrl(url: string): UrlParser {
+    this.url = new URL(url)
+    if (this.url.hostname === "github.com") {
+      this.organization = this.url.pathname.split('/')[1]
+      this.project = this.url.pathname.split('/')[2]
+    } else if (this.url.hostname === "app.circleci.com") {
+      this.organization = this.url.pathname.split('/')[3]
+      this.project = this.url.pathname.split('/')[4]
+    }
+    return this;
+  }
+
+  public isParseable(): boolean {
+    return !!(this.organization && this.project)
+  }
+
+  public getGitHubProjectHome(): string {
+    return `https://github.com/${this.organization}/${this.project}`
+  }
+
+  public getCircleCiProjectHome(): string {
+    return `https://app.circleci.com/pipelines/github/${this.organization}/${this.project}?branch=main`
+  }
 }
 
 const Button = (props) => {
   return (
     <a href={props.href} className={props.className}>{props.name}</a>
+  )
+}
+
+const NavigationBar = () => {
+  let currentUrl = location.href
+  const urlParser = new UrlParser().setUrl(currentUrl)
+
+  const [githubUrl, setGithubUrl] = useState(urlParser.getGitHubProjectHome());
+  const [circleCiUrl, setCircleCiUrl] = useState(urlParser.getCircleCiProjectHome());
+
+  setInterval(() => {
+    if (location.href !== currentUrl) {
+      currentUrl = location.href;
+      urlParser.setUrl(currentUrl)
+      setGithubUrl(urlParser.getGitHubProjectHome())
+      setCircleCiUrl(urlParser.getCircleCiProjectHome())
+    }
+  }, 500);
+
+
+  return (
+    <div className="navigation-bar" style={{}}>
+      <Button name="Github" href={githubUrl} className="github button" />
+      <Button name="CircleCI" href={circleCiUrl} className="circleci button" />
+    </div>
   )
 }
 
